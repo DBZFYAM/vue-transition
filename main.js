1376dcb4ac7bba345287ce8830ad9e75e0931099
vingojw <vingojw@gmail.com>
2016-03-14 18:36:03 +0800

    u

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 // Define some components
-var Foo = Vue.extend({
-    template: '<p>This is foo!</p>'
+var page1 = Vue.extend({
+    template: '<div class="p1">页面一<br/>页面一<br/>页面一<br/>页面一<br/></div>'
 })
 
-var Bar = Vue.extend({
-    template: '<p>This is bar!</p>'
+var page2 = Vue.extend({
+    template: '<div class="p2">页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/></div>'
 })
+var page3 = Vue.extend({
+    template: '<div class="p3">页面三<br/>页面三<br/>页面三<br/>页面三<br/></div>'
+})
+
+var page4 = Vue.extend({
+    template: '<div class="p4">页面四<br/>页面四<br/>页面四<br/>页面四<br/>页面四<br/></div>'
+})
+
+var page5 = Vue.extend({
+    template: '<div class="p5">页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/></div>'
+})
+
+
 
 // The router needs a root component to render.
 // For demo purposes, we will just use an empty one
@@ -13,7 +26,8 @@ var Bar = Vue.extend({
 var App = Vue.extend({
     data:function(){
         return {
-            transition:'page'
+            transition:'page',
+            changePage:false
         }
     }
 })
@@ -29,15 +43,39 @@ var router = new VueRouter()
 // Vue.extend(), or just a component options object.
 // We'll talk about nested routes later.
 router.map({
-    '/foo': {
-        component: Foo
+    '/page1': {
+        component: page1
     },
-    '/bar': {
-        component: Bar
+    '/page2': {
+        component: page2
+    },
+    '/page3': {
+        component: page3
+    },
+    '/page4': {
+        component: page4
+    },
+    '/page5': {
+        component: page5
     }
 })
 
 // Now we can start the app!
 // The router will create an instance of App and mount to
 // the element matching the selector #app.
-router.start(App, '#app')
+router.start(App, '#app');
+
+
+Vue.transition('page', {
+  beforeEnter: function (el) {
+    console.log('1');
+    router.app.changePage = true;
+  },
+  enter:function(){
+    console.log('2');
+  },
+  afterEnter: function (el) {
+    console.log('3');
+    router.app.changePage = false;
+  }
+});
