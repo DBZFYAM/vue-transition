4f8b8e4816ce7c5d2b5e5ae97665be637201d1df
vingojw <vingojw@gmail.com>
2016-03-15 12:56:04 +0800

    u

--- main.js
+++ main.js
@@ -1,81 +1,106 @@
 // Define some components
-var page1 = Vue.extend({
-    template: '<div class="p1">页面一<br/>页面一<br/>页面一<br/>页面一<br/></div>'
+var home = Vue.extend({
+    template: '<div class="p1">home<br/>home<br/>home<br/>homehome<br/>home<br/>home<br/>homehome<br/>home<br/>home<br/>homehome<br/>home<br/>home<br/>homehome<br/>home<br/>home<br/>homehome<br/>home<br/>home<br/>homehome<br/>home<br/>home<br/>homehome<br/>home<br/>home<br/>homehome<br/>home<br/>home<br/>homehome<br/>home<br/>home<br/>homehome<br/>home<br/>home<br/>homehome<br/>home<br/>home<br/>homehome<br/>home<br/>home<br/>homehome<br/>home<br/>home<br/>homehome<br/>home<br/>home<br/>homehome<br/>home<br/>home<br/>home</div>'
 })
 
 var page2 = Vue.extend({
-    template: '<div class="p2">页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/></div>'
+    template: '<div class="p2">页面二1<br/>页面二<br/>页面二2<br/>页面二3<br/>页面二4<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二<br/>页面二</div>'
 })
+
 var page3 = Vue.extend({
-    template: '<div class="p3">页面三<br/>页面三<br/>页面三<br/>页面三<br/></div>'
+    template: '<div class="p3">页面三<br/>页面三<br/>页面三<br/>页面三页面三<br/>页面三<br/>页面三<br/>页面三页面三<br/>页面三<br/>页面三<br/>页面三页面三<br/>页面三<br/>页面三<br/>页面三页面三<br/>页面三<br/>页面三<br/>页面三页面三<br/>页面三<br/>页面三<br/>页面三页面三<br/>页面三<br/>页面三<br/>页面三</div>'
 })
 
 var page4 = Vue.extend({
-    template: '<div class="p4">页面四<br/>页面四<br/>页面四<br/>页面四<br/>页面四<br/></div>'
+    template: '<div class="p4">页面四<br/>页面四<br/>页面四<br/>页面四<br/>页面四<br/>页面四<br/>页面四<br/>页面四<br/>页面四<br/>页面四<br/>页面四<br/>页面四<br/>页面四<br/>页面四<br/>页面四<br/>页面四<br/>页面四<br/>页面四<br/>页面四<br/>页面四<br/>页面四<br/>页面四<br/>页面四<br/>页面四<br/>页面四<br/>页面四<br/>页面四<br/>页面四<br/>页面四<br/>页面四<br/>页面四<br/>页面四<br/>页面四<br/>页面四<br/>页面四</div>'
 })
 
 var page5 = Vue.extend({
-    template: '<div class="p5">页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/></div>'
+    template: '<div class="p5">页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五<br/>页面五</div>'
 })
 
 
-
-// The router needs a root component to render.
-// For demo purposes, we will just use an empty one
-// because we are using the HTML as the app template.
 var App = Vue.extend({
     data:function(){
         return {
-            transition:'page',
+            effect:'next',
             changePage:false
         }
     }
 })
 
-// Create a router instance.
-// You can pass in additional options here, but let's
-// keep it simple for now.
-var router = new VueRouter()
 
-// Define some routes.
-// Each route should map to a component. The "component" can
-// either be an actual component constructor created via
-// Vue.extend(), or just a component options object.
-// We'll talk about nested routes later.
+var router = new VueRouter();
+
+
+//过渡代码放到最下面的时候，有时候并不会执行钩子函数的方法，放到这里就可以了。
+Vue.transition('next', {
+  beforeEnter: function (el) {
+    router.app.changePage = true;
+  },
+  enter:function(){
+  },
+  afterEnter: function (el) {
+    router.app.changePage = false;
+  }
+});
+Vue.transition('prev', {
+  beforeEnter: function (el) {
+    router.app.changePage = true;
+  },
+  enter:function(){
+  },
+  afterEnter: function (el) {
+    router.app.changePage = false;
+  }
+});
+
 router.map({
-    '/page1': {
-        component: page1
+    '/': {
+        name:'home',
+        component: home
     },
     '/page2': {
+        name:'page2',
         component: page2
     },
     '/page3': {
+        name:'page3',
         component: page3
     },
     '/page4': {
+        name:'page4',
         component: page4
     },
     '/page5': {
+        name:'page5',
         component: page5
     }
 })
 
-// Now we can start the app!
-// The router will create an instance of App and mount to
-// the element matching the selector #app.
-router.start(App, '#app');
+//全局放一个routlist
 
-
-Vue.transition('page', {
-  beforeEnter: function (el) {
-    console.log('1');
-    router.app.changePage = true;
-  },
-  enter:function(){
-    console.log('2');
-  },
-  afterEnter: function (el) {
-    console.log('3');
-    router.app.changePage = false;
-  }
+window.routeList = [];
+router.beforeEach(function(transition){
+    if(routeList.length > 1 && transition.to.name==routeList[routeList.length-2]){
+        router.app.effect='prev';//返回
+        routeList.splice(routeList.length-1,1);
+        setTimeout(function(){
+            //这里加上延迟是要在afterEach之后在执行
+          transition.next()
+        },15);
+        return;
+    }
+    router.app.effect='next';//前进
+    routeList.push(transition.to.name);
+    transition.next();
 });
+
+router.afterEach(function(transition){
+    //这里必须要加上， 此钩子函数会在beforeEach之后马上执行,没有这句会导致
+    //当你浏览顺序为，首页->页面二->页面三
+    //此时点击页面二正常返回，然后再次选择页面三，本应该是前进，结果还是返回。
+    router.app.effect='next';//重置前进
+});
+
+router.start(App, '#app');
